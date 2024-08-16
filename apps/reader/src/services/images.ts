932f027dd3b460cf5d67d245323fe2f613dde6a1
quynh.hoang <vquynh08t4@gmail.com>
2024-08-17 03:46:21 +0700

    fix eslint

--- apps/reader/src/services/images.ts
+++ apps/reader/src/services/images.ts
@@ -2,8 +2,8 @@ import {AxiosPromise} from 'axios';
 
 import { api } from "./httpClient";
 
-export const generationsApi = (request: any): AxiosPromise<any> => {
-    const _request = {
+export const generationsApi = (_request: any): AxiosPromise<any> => {
+    const request = {
         "height": 384,
         "modelId": "6b645e3a-d64f-4341-a6d8-7a3690fbf042",
         "prompt": "Demen the cricket stands upright on its two hind legs, proudly occupying the foreground in front of the cave opening, which frames a serene view of the tranquil pond in the distance. The entrance of the cave is flanked by sparse, vibrant green blades of grass that grow in scattered tufts, their delicate tips swaying gently in the breeze. Demen s body is a warm, earthy brown color with a subtle sheen, and its large, round eyes shine like polished onyx. Its antennae, long and slender, twitch slightly as it gazes out at the ponds glassy surface, which reflects the soft, feathery clouds drifting lazily across the sky. The atmosphere is peaceful, with a sense of stillness and anticipation, as if Demen is waiting for something to emerge from the caves depths or watching for a ripple to disturb the ponds mirrored calm.",
@@ -11,7 +11,7 @@ export const generationsApi = (request: any): AxiosPromise<any> => {
        "num_images": 1
     }
     const endPoint = '/v1/generations';
-    return api.post(endPoint, _request);
+    return api.post(endPoint, request);
 };
   
 export const getImageFromGenerationId = (generationId: string):  AxiosPromise<any> => {
