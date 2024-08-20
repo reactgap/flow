32bb4adea7b635d90fdcad6d39090c4c0e29732c
pnhung177 <pnhung177@gmail.com>
2024-08-20 14:44:19 +0700

    Add the prompts for 4 sections in the Truyen_co_Andersen book

--- apps/reader/src/datastore/index.ts
+++ apps/reader/src/datastore/index.ts
@@ -9,6 +9,11 @@ const promptTotto00: any = "Demen the cricket stands upright on its two hind leg
 
 const promptTotto01: any = "Demen the cricket stands upright on its two hind legs, proudly occupying the foreground in front of the cave opening, which frames a serene view of the tranquil pond in the distance. The entrance of the cave is flanked by sparse, vibrant green blades of grass that grow in scattered tufts, their delicate tips swaying gently in the breeze. Demen s body is a warm, earthy brown color with a subtle sheen, and its large, round eyes shine like polished onyx. Its antennae, long and slender, twitch slightly as it gazes out at the ponds glassy surface, which reflects the soft, feathery clouds drifting lazily across the sky. The atmosphere is peaceful, with a sense of stillness and anticipation, as if Demen is waiting for something to emerge from the caves depths or watching for a ripple to disturb the ponds mirrored calm.";
 
+const prompt201: any = "The girl's name is Daisy, weak and shy, pale skin, about 8-10 years old, in a thin and worn light blue dress, her shoeless feet are cold and shaking, shoulder-length brown hair unkempt. Her eyes shone with hope as she clutched the small rusty box of tiny matches to her chest, the fragile box decorated with old labels and faded lettering, the matches protruding from the opened lid, radiating a faint warm light in the cold darkness in night snowy on a busy street.";
+const prompt202: any = "The girl's name is Daisy, weak and shy, pale skin, about 8-10 years old, in a thin and worn light blue dress, her shoeless feet are cold and shaking, shoulder-length brown hair unkempt. Her eyes shone with hope as she clutched the small rusty box of tiny matches to her chest, the fragile box decorated with old labels and faded lettering, the matches protruding from the opened lid, radiating a faint warm light in the cold darkness. Daisy trembled and crouched against the mossy wall at the corner of the street to avoid the cold wind and the thick, wet snow. She picked up a match and struck it against the brick wall. The match lit up, illuminating Daisy's face in the darkness in night snowy on a busy street.";
+const prompt203: any = "On a cold winter night, snow fell gently, covering the street white. A little girl named Daisy, about 8-10 years old, stood in a corner between two old red houses. She was a shy, delicate child with pale skin, wearing a light blue dress that looked old and fragile, not enough to shield her from the cold air. The old shoes did not help keep her feet warm and made her shiver a bit from the cold. Daisy's shoulder-length brown hair was messy, swaying gently in the winter wind. She carried a leather crossbody bag filled with a box of matches on her shoulder. In her slender hand, Daisy held a match that emitted a gentle light, shining a warm glow on her smiling face. In front of her was an old woman with a hunchback, gray hair, a smiling face with many wrinkles, and her arms spread wide.";
+const prompt204: any = "The girl named Daisy, weak and shy, with pale skin, about 8-10 years old, wearing a thin, worn light blue dress, her feet in cold shoes shaking, her shoulder-length brown hair messy. Daisy was lying with her eyes closed as if sleeping, her arms spread out to the sides, one hand holding a burned match, on the cold ground, in a corner between two red houses, snow was falling outside. , the light shined on her, next to her were scattered matchboxes and on the ground were two burned matches. There were two women wearing warm fur dresses and a man wearing a warm suit standing on the side of the road and they were pointing at her with a whispering look.";
+
 const datastore: any = {
     "De-men-phieu-luu-ky.epub": {
         "/6/6": {
@@ -61,6 +66,29 @@ const datastore: any = {
                     paragraph: "/4/16",
                     prompt: promptTotto01,
                 }
+            ]
+        }
+    },
+    "Truyen_co_Andersen.epub": {
+        "/6/10": {
+            prompt: prompt201,
+            paragraphs: [
+                {
+                    paragraph: "/4/18",
+                    prompt: prompt201
+                },
+                {
+                    paragraph: "/4/32",
+                    prompt: prompt202
+                },
+                {
+                    paragraph: "/4/50",
+                    prompt: prompt203
+                },
+                {
+                    paragraph: "/4/60",
+                    prompt: prompt204
+                },
             ]
         }
     },
